4c917338a5c05c45a2e70161f3aa9ab165a7dbb9
J <59713685+josevflores911@users.noreply.github.com>
2023-06-16 21:38:07 -0300

    mostrando videos

---
+++ js/crearVIdeo.js
@@ -0,0 +1,16 @@
+import { conectaAPI } from "./conectaAPI.js";
+
+const formulario = document.querySelector("[data-formulario]");
+
+async function crearVideo(){
+    const imagen= document.querySelector("[data-imagen]");
+    const url = document.querySelector("[data-url]");
+    const titulo=document.querySelector("[data-titulo").value;
+    const descripcion = Math.floor(Math.random*10).toString();
+
+    await conectaAPI.crearVideo(titulo,descripcion,url,imagen)
+
+    window.location.href="../pages/envio-concluido.html"
+}
+
+formulario,addEventListener("submit",evento=>crearVideo(evento));
